0fcd774143c279be62137a8caa48c7b3551d38fb
Thomas Kefer <tkefer@tsn.at>
2021-10-25 17:04:05 +0200

    Added all res-files

--- ModifyColors/ImageManipulator.cs
+++ ModifyColors/ImageManipulator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Drawing;
 using System.Drawing.Imaging;
 
@@ -5,56 +6,221 @@ namespace ModifyColors
 {
     public class ImageManipulator
     {
-        public void RgbToGrayscale(ref Color[,] pixels)
+        public const int INTENSITY_LAYER_NUMBER = 256;
+
+        public void RgbToGrayscale(ref Color[,] pixels, ref Bitmap img)
         {
             for (var i = 0; i < pixels.GetLength(0); ++i)
             {
                 for (var j = 0; j < pixels.GetLength(1); ++j)
                 {
-                    var p = pixels[i, j];
+                    var p = img.GetPixel(i, j);
                     var gray = (p.R + p.G + p.B) / 3;
-                    
+
                     pixels[i, j] = Color.FromArgb(gray, gray, gray);
                 }
             }
         }
-        
-        public int Otsu(ref Bitmap bmp)
+
+        private int[] colors2dToInt1d(ref Color[,] colors)
         {
-            byte t=0;
-            float[] vet=new float[256];
-            int[] hist=new int[256];
-            vet.Initialize();
-
-            float p1,p2,p12;
-            int k;
-
-            var bmData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height),
-                ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
-            unsafe
+            var pixels = new int[colors.Length];
+            var counter = 0;
+            foreach (var i in colors)
             {
-                byte* p = (byte*)(void*)bmData.Scan0.ToPointer();
+                pixels[counter++] = i.B;
+            }
 
-                getHistogram(p,bmp.Width,bmp.Height,bmData.Stride, hist);
+            return pixels;
+        }
 
-        
-                for (k = 1; k != 255; k++)
+        private void calculateHistogram(ref int[] pixels, int pixelCount, ref int[] hist)
+        {
+            Array.Clear(hist, 0, hist.Length);
+
+            for (var i = 0; i < pixelCount; ++i)
+            {
+                ++hist[pixels[i]];
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pixels"></param>
+        /// <param name="pixelCount"></param>
+        /// <returns></returns>
+        private int calculateIntensitySum(ref int[] pixels, int pixelCount)
+        {
+            var sum = 0;
+            foreach (var pixel in pixels)
+            {
+                sum += pixel;
+            }
+
+            return sum;
+        }
+
+        public void TryWithEmgu(Bitmap bmp)
+        {
+            //TODO : Try to understand EmguCV
+        }
+
+        public static long[] CreateOwnHistogram(Bitmap img)
+        {
+            var hist_gray = new long[256];
+            var hist_red = new long[256];
+            var hist_green = new long[256];
+            var hist_blue = new long[256];
+
+            for (var x = 0; x < img.Width; ++x)
+            {
+                for (var y = 0; y < img.Height; ++y)
                 {
-                    p1 = Px(0, k, hist);
-                    p2 = Px(k + 1, 255, hist);
-                    p12 = p1 * p2;
-                    if (p12 == 0) 
-                        p12 = 1;
-                    float diff=(Mx(0, k, hist) * p2) - (Mx(k + 1, 255, hist) * p1);
-                    vet[k] = (float)diff * diff / p12;
-            
+                    var currP = img.GetPixel(x, y);
+                    hist_blue[currP.B]++;
+                    hist_green[currP.G]++;
+                    hist_red[currP.R]++;
+                    var tempBrightness = (currP.B + currP.G + currP.R) / 3;
+                    hist_gray[tempBrightness]++;
                 }
             }
-            bmp.UnlockBits(bmData);
 
-            t = (byte)findMax(vet, 256);
+            return hist_gray;
+        }
 
-            return t;
+        private long CreateIntensitySum(long[] hist)
+        {
+            var sum = 0L;
+            foreach (var x in hist)
+            {
+                sum += x;
+            }
+
+            return sum;
+        }
+
+        public double OtsuOwn(ref Bitmap img)
+        {
+            // create the histogram of the image
+            var hist = CreateOwnHistogram(img);
+
+            var bestThresh = 0;
+
+            var lowerClassPixelCount = 0;
+            var lowerClassIntensitySum = CreateIntensitySum(hist);
+            for (var thresh = 0; thresh < hist.Length; ++thresh)
+            {
+                
+            }
+
+            return 0.0d;
+        }
+
+        /// <summary>Die Funktion gibt den Binarisierungsschwellenwert für ein Halbtonbild mit einer Gesamtanzahl von Pixeln zurück.</summary>
+        /// <param name="image">Enthält die Intensität des Bildes von 0 bis einschließlich 255.</param>
+        public double Otsu(ref Bitmap bmp)
+        {
+            var colors = new Color[bmp.Width, bmp.Height];
+            RgbToGrayscale(ref colors, ref bmp);
+            var pixels = colors2dToInt1d(ref colors);
+
+            var histogram = new int[256];
+            calculateHistogram(ref pixels, pixels.Length, ref histogram);
+
+            // Wird benötigt, um den Unterschied in den Varianzen zwischen den Klassen schnell neu zu berechnen
+            var allPixelCount = pixels.Length;
+            var allIntensitySum = calculateIntensitySum(ref pixels, pixels.Length);
+
+            var bestTresh = 0;
+            var bestSigma = 0.0d;
+
+            var firstClassPixelCount = 0;
+            var firstClassIntensitySum = 0;
+
+            // Schleife über die zwei Klassen
+            // thresh < INTENSITY_LAYER_NUMBER-1, weil bei 255 ein int-overflow passiert
+            for (var thresh = 0; thresh < histogram.Length; ++thresh)
+            {
+                firstClassPixelCount += histogram[thresh];
+                firstClassIntensitySum += thresh * histogram[thresh];
+
+                var firstClassProb = firstClassPixelCount / (double)allPixelCount;
+                var secondClassProb = 1.0d - firstClassProb;
+
+                var firstClassMean = firstClassIntensitySum / (double)firstClassPixelCount;
+                var secondClassMean = (allIntensitySum - firstClassIntensitySum) /
+                                      (double)(allPixelCount - firstClassPixelCount);
+
+                var meanDelta = firstClassMean - secondClassMean;
+
+                var sigma = firstClassProb * secondClassProb * meanDelta * meanDelta;
+
+                if (sigma > bestSigma)
+                {
+                    bestSigma = sigma;
+                    bestTresh = thresh;
+                }
+            }
+
+            return bestTresh/255.0d;
+        }
+
+        public double ThresholdWithStackOverflow(ref Bitmap img)
+        {
+            var avgBright = 0.0d;
+            for (var i = 0; i < img.Width; ++i)
+            {
+                for (var j = 0; j < img.Height; ++j)
+                {
+                    avgBright += img.GetPixel(i, j).GetBrightness();
+                }
+            }
+
+            avgBright = avgBright / (img.Height * img.Width);
+            avgBright = avgBright < .3d ? .3 : avgBright;
+            return avgBright > .7d ? .7d : avgBright;
+        }
+
+        /// <summary>
+        /// Applies the threshold onto a new Bitmap.
+        /// </summary>
+        /// <param name="img">From where the pixels are read</param>
+        /// <param name="thresh">The threshold to use</param>
+        /// <returns>A Bitmap with the threshold applied orignal</returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Bitmap ApplyThreshold(ref Bitmap img, double thresh)
+        {
+            /*if ((img.PixelFormat & PixelFormat.Indexed) == PixelFormat.Indexed)
+            {
+                Console.WriteLine("Is indexed, need to abort (for now)");
+                throw new NotImplementedException("Cannot process indexed images yet");
+            }*/
+
+            var tempImg = new Bitmap(img.Width, img.Height);
+            using (var g = Graphics.FromImage(tempImg))
+            {
+                for (var i = 0; i < img.Width; ++i)
+                {
+                    for (var j = 0; j < img.Height; ++j)
+                    {
+                        var p = img.GetPixel(i, j);
+
+                        if (p.GetBrightness() >= p.GetBrightness() * thresh)
+                        {
+                            p = Color.White;
+                        }
+                        else
+                        {
+                            p = Color.Black;
+                        }
+                        
+                        tempImg.SetPixel(i,j, p);
+                    }
+                }
+            }
+
+            return tempImg;
         }
     }
 }
--- ModifyColors/Program.cs
+++ ModifyColors/Program.cs
@@ -2,27 +2,48 @@
 using System.Drawing;
 using System.IO;
 using System.IO.Enumeration;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+
 
 namespace ModifyColors
 {
     class Program
     {
+        public const string imageSrc_wikipedia =
+            @"C:\Users\Baumbart13\RiderProjects\TestThings\ModifyColors\res\Image_processing_pre_otsus_algorithm.jpg";
+
+        public const string imageSrc_own = @"C:\Users\Baumbart13\RiderProjects\TestThings\ModifyColors\res\Unbenannt.png";
+
+        public const string imageDest_wikipedia = @"C:\Users\Baumbart13\Desktop\Unnamed_wiki.png";
+        public const string imageDest_own = @"C:\Users\Baumbart13\Desktop\Unnamed_own.png";
+
         static void Main(string[] args)
         {
-            Bitmap img = new Bitmap("%HOME%/Desktop/second.png");
             var mp = new ImageManipulator();
 
-            var pixels = new Color[img.Height, img.Width];
-            for (var x = 0; x < img.Width; ++x)
-            {
-                for (var y = 0; y < img.Height; ++y)
-                {
-                    pixels[x, y] = img.GetPixel(x, y);
-                }
-            }
+            Bitmap img = new Bitmap(imageSrc_wikipedia);
             
-            mp.RgbToGrayscale(ref pixels);
-            mp.Otsu(ref pixels);
+            var otsu = mp.Otsu(ref img);
+            var avgBright = mp.ThresholdWithStackOverflow(ref img);
+            var otsuOwn = mp.OtsuOwn(ref img);
+            
+            img = mp.ApplyThreshold(ref img, otsu);
+            img.Save(imageDest_wikipedia);
+            Console.WriteLine("First image saved");
+
+            // another image
+            img = new Bitmap(imageSrc_own);
+            
+            otsu = mp.Otsu(ref img);
+            avgBright = mp.ThresholdWithStackOverflow(ref img);
+            otsuOwn = mp.OtsuOwn(ref img);
+            
+            img = mp.ApplyThreshold(ref img, otsu);
+            img.Save(imageDest_own);
+            Console.WriteLine("Second image saved");
         }
     }
 }
