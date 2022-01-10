809c8e45c5a93e93052521d0e310414e9f695319
Baumbart13 <thomaskefer0109@gmail.com>
2022-01-11 00:21:39 +0100

    Added Client tutorial code for NetMQ

---
+++ Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NetMQ;
+using NetMQ.Sockets;
+
+static class Program
+{
+    public static void Main()
+    {
+        const string msg = "Hello";
+        Console.WriteLine("Connecting to hello world server…");
+        using(var requester = new RequestSocket())
+        {
+            requester.Connect("tcp://localhost:5555");
+
+            int requestNumber;
+            for (requestNumber = 0; requestNumber != 10; requestNumber++)
+            {
+                Console.WriteLine($"[{DateTime.Now.Date}][{DateTime.Now.TimeOfDay}]: Sending \"{msg}\" with request number \"{requestNumber}\" to Server...");
+                requester.SendFrame(msg);
+                string str = requester.ReceiveFrameString();
+                Console.WriteLine($"[{DateTime.Now.Date}][{DateTime.Now.TimeOfDay}]: Received \"{str}\" with request number \"{requestNumber}\" from Server");
+            }
+        }
+    }    
+}
