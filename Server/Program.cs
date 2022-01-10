588c37c7a556599b6a253b2c3808e1696d8ecec2
Baumbart13 <thomaskefer0109@gmail.com>
2022-01-11 00:21:47 +0100

    Added Server tutorial code for NetMQ

---
+++ Server/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using NetMQ;
+using NetMQ.Sockets;
+
+static class Program
+{
+    public static void Main()
+    {        
+        using (var responder = new ServerSocket())
+        {
+            responder.Bind("tcp://*:5555");
+
+            while (true)
+            {
+                var str = responder.ReceiveBytesAsync();
+                Console.WriteLine($"[{DateTime.Now.Date}][{DateTime.Now.TimeOfDay}]: Received \"{str}\" from Client");
+                Thread.Sleep(1000);  //  Do some 'work'
+            }
+        }
+    }
+}
