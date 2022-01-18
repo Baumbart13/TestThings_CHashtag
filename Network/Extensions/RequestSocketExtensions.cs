e34adabc5c69a821a71de90447b5450a5b41eedd
Baumbart13 <thomaskefer0109@gmail.com>
2022-01-19 00:53:43 +0100

    Did some work

---
+++ Network/Extensions/RequestSocketExtensions.cs
@@ -0,0 +1,11 @@
+using NetMQ;
+
+namespace Network.Extensions;
+
+public static class RequestSocketExtensions
+{
+    public static void SendFrame(this IOutgoingSocket socket, Message message, bool more = false)
+    {
+        socket.S
+    }
+}
---
+++ Network/Extensions/ResponseSocketExtensions.cs
@@ -0,0 +1,9 @@
+namespace Network.Extensions;
+
+public static class ResponseSocketExtensions
+{
+    public static void SendFrame(Message message)
+    {
+        
+    }
+}
