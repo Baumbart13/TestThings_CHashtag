079b803ea824f2c1223a2daa93b307f1c992e2f8
Baumbart13 <thomaskefer0109@gmail.com>
2022-01-19 23:36:25 +0100

    Added new structure of sending messages

--- Network/Extensions/RequestSocketExtensions.cs
+++ Network/Extensions/RequestSocketExtensions.cs
@@ -6,7 +6,17 @@ public static class RequestSocketExtensions
 {
     public static void SendMessage(this IOutgoingSocket socket, Message message)
     {
-        socket.SendMessage(message.ToNetMqMessage());
+        // - MessageType
+        // - Number of MessageArgument
+        // - for each MessageArgument
+        // --- MessageArgumentType
+        // --- MessageArgumentValue
+
+        Int32 msgType = (Int32)message.MessageType;
+        Int32 msgNoArgs = (Int32)message.Content.Count;
+        NetMQ.NetMQMessage msg = new NetMQMessage();
+        msg.AppendEmptyFrame();
+        msg[0].
     }
 
     public static IOutgoingSocket SendMoreMessages(this IOutgoingSocket socket, Message message)
