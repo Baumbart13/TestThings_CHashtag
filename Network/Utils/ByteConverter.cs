0e48e233a43072a4d3498132f943e011c4707143
Baumbart13 <tkefer@tsn.at>
2022-02-19 13:48:33 +0100

    Finished ByteConverter. Can convert all primitive types to array of bytes and vice versa via Extension-Class.

--- Network/Utils/ByteConverter.cs
+++ Network/Utils/ByteConverter.cs
@@ -1,8 +1,23 @@
+using System.Text;
+
 namespace Network.Utils;
 
 public static class ByteConverter
 {
 #region ToByte
+
+    public static byte[] ToBytes(this string str)
+    {
+        var b = new byte[str.Length*2];
+        for (int strI = 0, bI = 0; strI < str.Length; strI += 1, bI += 2)
+        {
+            var c = str[strI].ToBytes();
+            b[bI] = c[0];
+            b[bI + 1] = c[1];
+        }
+
+        return b;
+    }
 
     public static byte[] ToBytes(this int i)
     {
@@ -58,10 +73,10 @@ public static class ByteConverter
 
     public static int ToInt32(this byte[] b)
     {
-        return (b[0] << 24) |
-               (b[1] << 16) |
-               (b[2] << 8) |
-               b[3];
+        return ((int)(b[0]) << 24) |
+               ((int)(b[1]) << 16) |
+               ((int)(b[2]) << 8) |
+               (int)(b[3]);
     }
 
     public static short ToInt16(this byte[] b)
@@ -77,20 +92,32 @@ public static class ByteConverter
 
     public static char ToCharacter(this byte[] b)
     {
-        return (char)((char)(b[0] << 8) |
-                       (char)(b[1]));
+        return (char)b.ToInt16();
+    }
+
+    public static string ArrayToString(this byte[] b)
+    {
+        var sb = new StringBuilder((int)Math.Ceiling(b.Length * 0.5));
+
+        for (var i = 0; i < b.Length; i += 2)
+        {
+            var c = (char)(((short)b[i] << 8) | b[i+1]);
+            sb.Append(c);
+        }
+        
+        return sb.ToString();
     }
 
     public static long ToInt64(this byte[] b)
     {
-        return (long)((long)((long)b[0] >> 56) |
-            (long)((long)b[1] >> 48) |
-            (long)((long)b[2] >> 40) |
-            (long)((long)b[3] >> 32) |
-            (long)((long)b[4] >> 24) |
-            (long)((long)b[5] >> 16) |
-            (long)((long)b[6] >> 8) |
-            (long)(long)b[7]);
+        return (long)((long)((long)(b[0]) << 56) |
+            (long)((long)(b[1]) << 48) |
+            (long)((long)(b[2]) << 40) |
+            (long)((long)(b[3]) << 32) |
+            (long)((long)(b[4]) << 24) |
+            (long)((long)(b[5]) << 16) |
+            (long)((long)(b[6]) << 8) |
+            (long)(b[7]));
     }
 
 #endregion
